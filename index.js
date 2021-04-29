b41804b8c1b9383f8dc70e42c69012adc9827450
igalv <vilenskyigal@gmail.com>
2021-04-29 15:29:47 +0200

    css and keybord function

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const character = document.querySelector("#character");
 const block = document.querySelector("#block");
+const body = document.querySelector("body");
+
+let StandPoint = 0;
+
+function jumpKey() {
+  StandPoint = 100;
+  character.style.top = StandPoint + "px";
+  setTimeout(() => {
+    character.style.top = "100px";
+  }, 200);
+
+  setTimeout(() => {
+    character.style.top = "150px";
+  }, 500);
+}
+
+function move(e) {
+  if (e.code == "Space") jumpKey();
+  console.log(e.code);
+}
 
 function jump() {
   character.classList.add("animate");
@@ -19,6 +39,10 @@ const dead = setInterval(() => {
     block.style.animation = "none";
     block.style.display = "none";
 
-    alert("Game over!");
+    alert(`Game over! 
+
+     Please refresh the page to continue.`);
   }
 }, 10);
+
+body.addEventListener("keypress", move);
