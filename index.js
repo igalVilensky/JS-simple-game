cee657087d0145320593108bfee15211e4591ba6
igalv <vilenskyigal@gmail.com>
2021-04-29 14:44:54 +0200

    animation and dead function

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function jump() {
     character.classList.remove("animate");
   }, 500);
 }
+
+const dead = setInterval(() => {
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+
+    alert("Game over!");
+  }
+}, 10);
